d9947e976c4e7391edd7d743c2cca6e6fbe7a15c
Mandy1804 <amandarccamargo@gmail.com>
2025-05-13 22:19:27 -0300

    teste

---
+++ js/cadastro.js
@@ -0,0 +1,15 @@
+// fazer o cadastro
+const botao = document.getElementById ('btnCadastrar');
+const listaUsuariosCadastrados =[];
+// cadastrar
+botao.addEventListener('click', function(){
+    const objUsuario = {
+        usuario: document.getElementById('usuario').value,
+        senha: document.getElementById('senha').value
+    };
+    listaUsuariosCadastrados.push(objUsuario);
+    const listaJson = JSON.stringify(listaUsuariosCadastrados);
+    localStorage.setItem('usuario', listaJson);
+}
+
+);
